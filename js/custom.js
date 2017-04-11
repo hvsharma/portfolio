11b97f2a5cdc423b10a94c208697e1146bab9615
hvsharma <hvsharma_learning@outlook.com>
2017-04-11 17:00:21 +0530

    Finalized Banner

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(function(){
+    $("#btn-fb").click(function(){
+        window.open("https://www.facebook.com/hvsharma","_blank");
+    });
+
+    $("#btn-gh").click(function(){
+        window.open("https://www.github.com/hvsharma","_blank");
+    });
+
+    $("#btn-lin").click(function(){
+        window.open("https://www.linkedin.com/in/sharmahv","_blank");
+    });
+
+    $("#btn-ig").click(function(){
+        window.open("https://www.instagram.com/sharma_hv","_blank");
+    });
+
+    $("#btn-twt").click(function(){
+        window.open("https://www.twitter.com/sharma_hv","_blank");
+    });   
+});
